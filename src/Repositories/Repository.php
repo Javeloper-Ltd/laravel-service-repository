54e52245d30f7da9a4bfca3bd1641c581ae5a856
Jack Mollart <jack@javeloper.co.uk>
2021-05-02 22:04:17 +0100

    Add in some of the default crud actions.

--- src/Repositories/Repository.php
+++ src/Repositories/Repository.php
@@ -5,6 +5,7 @@ namespace Jxckaroo\LaravelServiceRepository\Repositories;
 use Illuminate\Database\Eloquent\Model;
 use Jxckaroo\LaravelServiceRepository\Exceptions\Repositories\InvalidModelException;
 use Jxckaroo\LaravelServiceRepository\Repositories\Interfaces\RepositoryInterface;
+use Jxckaroo\LaravelServiceRepository\Traits\ModelCrudActions;
 
 /**
  * Class Repository
@@ -12,6 +13,8 @@ use Jxckaroo\LaravelServiceRepository\Repositories\Interfaces\RepositoryInterfac
  */
 abstract class Repository implements RepositoryInterface
 {
+    use ModelCrudActions;
+
     /**
      * @var Model
      */
@@ -37,7 +40,7 @@ abstract class Repository implements RepositoryInterface
     /**
      * @throws InvalidModelException
      */
-    public function resolveCorrectModel()
+    protected function resolveCorrectModel()
     {
         if (is_null($this->model)) {
             throw new InvalidModelException("Repository model cannot be null. Have you remembered to set \$model on your repository?");
@@ -48,7 +51,7 @@ abstract class Repository implements RepositoryInterface
          */
         $this->model = resolve($this->model);
 
-        if ($this->model instanceof Model) {
+        if (!$this->model instanceof Model) {
             throw new InvalidModelException("Repository model must be an instance of Illuminate\Database\Eloquent\Model.");
         }
     }
---
+++ src/Traits/ModelCrudActions.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Jxckaroo\LaravelServiceRepository\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Collection;
+
+/**
+ * Trait ModelCrudActions
+ * @package Jxckaroo\LaravelServiceRepository\Traits
+ */
+trait ModelCrudActions
+{
+    /**
+     * @var array $activeWith
+     */
+    protected $activeWith = [];
+
+    /**
+     * @var array $activeWithCount
+     */
+    protected $activeWithCount = [];
+
+    /**
+     * @return Collection
+     */
+    public function getAll(): Collection
+    {
+        return $this->buildQuery()->get();
+    }
+
+    /**
+     * @param $selectedKey
+     * @return Model|null
+     */
+    public function find($selectedKey): ?Model
+    {
+        return $this->buildQuery()->whereKey($selectedKey)->first();
+    }
+
+    /**
+     * @param array $with
+     * @return $this
+     */
+    public function with(array $with)
+    {
+        $this->activeWith = $with;
+        return $this;
+    }
+
+    /**
+     * @param array $withCount
+     * @return $this
+     */
+    public function withCount(array $withCount)
+    {
+        $this->activeWithCount = $withCount;
+        return $this;
+    }
+
+    /**
+     * @return Builder
+     */
+    public function buildQuery(): Builder
+    {
+        $freshQuery = $this->model::query();
+
+        if (count($this->activeWith) > 0) {
+            $freshQuery->with($this->activeWith);
+        }
+
+        if (count($this->activeWithCount) > 0) {
+            $freshQuery->withCount($this->activeWithCount);
+        }
+
+        return $freshQuery;
+    }
+}
